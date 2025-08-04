9abc838c78f652eb73ac22e5b253244c43dc949e
gminnimk <tvt1245@naver.com>
2025-08-04 18:08:46 +0900

    init commit

---
+++ src/main/java/be/url_backend/UrlBackendApplication.java
@@ -0,0 +1,13 @@
+package be.url_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UrlBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/url_backend/UrlBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package be.url_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
