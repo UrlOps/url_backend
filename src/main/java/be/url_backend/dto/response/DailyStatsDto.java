b15386da47792df131197104d5b31c6399a4be85
gminnimk <tvt1245@naver.com>
2025-08-13 18:56:46 +0900

    feat: 생성자(No,All) 어노테이션

--- src/main/java/be/url_backend/dto/response/DailyStatsDto.java
+++ src/main/java/be/url_backend/dto/response/DailyStatsDto.java
@@ -1,13 +1,17 @@
 package be.url_backend.dto.response;
 
 import be.url_backend.domain.DailyStats;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class DailyStatsDto {
     private LocalDate date;
     private Long count;
