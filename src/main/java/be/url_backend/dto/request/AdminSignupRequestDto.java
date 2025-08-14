ca3f110b5baf0a062552c39bc660b5c7110b2f76
gminnimk <tvt1245@naver.com>
2025-08-14 12:16:40 +0900

    fix: 요청 DTO Setter 명시 & customKey 제거

--- src/main/java/be/url_backend/dto/request/AdminSignupRequestDto.java
+++ src/main/java/be/url_backend/dto/request/AdminSignupRequestDto.java
@@ -2,10 +2,8 @@ package be.url_backend.dto.request;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Getter
-@Setter
 @NoArgsConstructor
 public class AdminSignupRequestDto {
     private String username;
--- src/main/java/be/url_backend/dto/request/UrlCreateRequestDto.java
+++ src/main/java/be/url_backend/dto/request/UrlCreateRequestDto.java
@@ -2,12 +2,9 @@ package be.url_backend.dto.request;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Getter
-@Setter
 @NoArgsConstructor
 public class UrlCreateRequestDto {
     private String originalUrl;
-    private String customKey;
 } 
