35d864bb7b0448687ca37a65300c31ae4fac407f
gminnimk <tvt1245@naver.com>
2025-08-16 15:44:02 +0900

    fix: 불필요한 쿼리 발생 => EntityGraph 제거

--- src/main/java/be/url_backend/repository/DailyStatsRepository.java
+++ src/main/java/be/url_backend/repository/DailyStatsRepository.java
@@ -2,7 +2,6 @@ package be.url_backend.repository;
 
 import be.url_backend.domain.DailyStats;
 import be.url_backend.domain.UrlMapping;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,6 +10,5 @@ import java.util.Optional;
 
 @Repository
 public interface DailyStatsRepository extends JpaRepository<DailyStats, Long> {
-    @EntityGraph(attributePaths = {"urlMapping"})
     Optional<DailyStats> findByUrlMappingAndDate(UrlMapping urlMapping, LocalDate date);
 } 
