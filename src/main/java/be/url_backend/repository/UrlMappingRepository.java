b54b6c8091bb8eb6f486d443dce7f034f825b88e
gminnimk <tvt1245@naver.com>
2025-08-18 14:04:40 +0900

    feat: URL redirection API – Ehcache 적용 성능 최적화

--- src/main/java/be/url_backend/repository/UrlMappingRepository.java
+++ src/main/java/be/url_backend/repository/UrlMappingRepository.java
@@ -2,9 +2,11 @@ package be.url_backend.repository;
 
 import be.url_backend.domain.UrlMapping;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.cache.annotation.Cacheable;
 
 import java.util.Optional;
 
 public interface UrlMappingRepository extends JpaRepository<UrlMapping, Long> {
+    @Cacheable(value = "url-mapping-cache", key = "#shortKey")
     Optional<UrlMapping> findByShortKey(String shortKey);
 }
