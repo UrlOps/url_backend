1316c74841f61343358f3c8016eb9f76b25fa2c5
gminnimk <tvt1245@naver.com>
2025-08-22 17:03:29 +0900

    del

--- src/main/java/be/url_backend/repository/ClickLogRepositoryCustom.java
+++
@@ -1,9 +0,0 @@
-package be.url_backend.repository;
-
-import be.url_backend.dto.response.DailyStatsDto;
-
-import java.util.List;
-
-public interface ClickLogRepositoryCustom {
-    List<DailyStatsDto> findDailyClickStats();
-} 
