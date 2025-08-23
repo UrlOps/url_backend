5d95f495c8673e93162fc5dfc5a6651744af45c5
gminnimk <tvt1245@naver.com>
2025-08-23 19:35:05 +0900

    fix: 관리자 화면 OSIV 활성화

--- src/main/java/be/url_backend/common/config/WebConfig.java
+++ src/main/java/be/url_backend/common/config/WebConfig.java
@@ -1,11 +1,19 @@
 package be.url_backend.common.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.support.OpenEntityManagerInViewInterceptor;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import jakarta.persistence.EntityManagerFactory;
+import lombok.RequiredArgsConstructor;
+
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
+
+    private final EntityManagerFactory entityManagerFactory;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
@@ -14,5 +22,14 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        OpenEntityManagerInViewInterceptor openEntityManagerInViewInterceptor = new OpenEntityManagerInViewInterceptor();
+        openEntityManagerInViewInterceptor.setEntityManagerFactory(entityManagerFactory);
+
+        registry.addWebRequestInterceptor(openEntityManagerInViewInterceptor)
+                .addPathPatterns("/api/admin/**");
     }
 } 
